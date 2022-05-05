bf7bf6dcf67d32d3361148397f7c522d6ec02e38
YAN LAZAROUSKI <fox29092000@gmail.com>
2022-05-05 16:18:51 +0300

    добавлена функциональнасть рандомного персонажа

--- marvel_starter/src/index.js
+++ marvel_starter/src/index.js
@@ -1,12 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/App';
-import MarvelService from './services/MarvelService';
 
 import './style/style.scss';
 
-const marvelservice = new MarvelService();
-marvelservice.getAllCharacters().then(res => console.log(res));
 
 ReactDOM.render(
   <React.StrictMode>
